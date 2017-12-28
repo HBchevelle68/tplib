2db63ccd5251bb1d7890292220795281ec948c06
hbchevelle68 <aportante10@gmail.com>
2017-12-28 08:46:36 -0800

    basictest 1 complete

--- basictest.c
+++ basictest.c
@@ -4,19 +4,19 @@
 
 #include "tplib.h"
 
-#define RED     "\x1b[91m"
-#define GREEN   "\x1b[92m"
-#define YELLOW  "\x1b[93m"
+#define RED           "\x1b[91m"
+#define GREEN         "\x1b[92m"
+#define YELLOW        "\x1b[93m"
 #define COLOR_RESET   "\x1b[0m"
 
-#define LOG_RED(X) printf("%s %s %s\n",RED, X, COLOR_RESET)
-#define LOG_GREEN(X) printf("%s %s %s\n",GREEN, X, COLOR_RESET)
-
+#define LOG_RED(X) printf("%s%s%s\n",RED, X, COLOR_RESET)
+#define LOG_GREEN(X) printf("%s%s%s\n",GREEN, X, COLOR_RESET)
+#define LOG_YELLOW(X) printf("%s%s%s\n",YELLOW, X, COLOR_RESET)
 
 static
 int btest_1(){
   struct threadpool_t *workers;
-  fprintf(stderr, "%s[+] Creating worker pool with %d threads %s\n",YELLOW, TESTTHREADS, COLOR_RESET);
+  fprintf(stderr, "%s[*] Creating worker pool with %d threads %s\n",YELLOW, TESTTHREADS, COLOR_RESET);
   if((workers = tpool_init(TESTTHREADS)) == NULL){
     LOG_RED("[-] Error on tpool_init");
     return 1;
@@ -30,6 +30,12 @@ int btest_1(){
 
 int main(){
     int ret;
+
+    #if (__SANITIZE_ADDRESS__)
+         LOG_YELLOW("[*] Running basictest compiled with address sanitizer");
+    #else
+         LOG_YELLOW("[*] Running basictest compiled with NO address sanitizer");
+    #endif
     if((ret = btest_1()) > 0){
       LOG_RED("[-] Basic Test 1 failure");
     }
